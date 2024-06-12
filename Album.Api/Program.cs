05d686e8d3c38df9af39f162ff115c1a86e0e3ce
Ceydag <100134625+Ceydag@users.noreply.github.com>
2024-06-12 15:34:56 +0200

    test

--- Album.Api/Program.cs
+++ Album.Api/Program.cs
@@ -1,5 +1,3 @@
-// note: namespace AlbumApi renamed from Album.Api to fix model Album namespace error.
-
 using Album.Api.Database;
 using Album.Api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -10,61 +8,53 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => {
-		options.AddDefaultPolicy(builder => {
-			builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
-		}); 
-	});
-	
-
-var Configuration = new ConfigurationBuilder()
-	.AddJsonFile("appsettings.json")
-	.Build();
-
-
-//Adding the database
-builder.Services.AddDbContext<Album.Api.Database.AlbumContext>(options =>
+builder.Services.AddCors(options =>
 {
-	options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+    });
 });
 
+var Configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
+// Adding the database
+builder.Services.AddDbContext<Album.Api.Database.AlbumContext>(options =>
+{
+    options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 app.UseCors(policy => policy
-				.AllowAnyMethod()
-				.AllowAnyHeader()
-				.AllowAnyOrigin());
-				
-				
-				
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowAnyOrigin());
+
 app.MapHealthChecks("/health");
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-
-				
 using (var scope = app.Services.CreateScope())
 {
-	var dbContext = scope.ServiceProvider.GetRequiredService<Album.Api.Database.AlbumContext>();
-	DBInitializer.Initialize(dbContext);
+    var dbContext = scope.ServiceProvider.GetRequiredService<Album.Api.Database.AlbumContext>();
+    DBInitializer.Initialize(dbContext);
 }
-
-
 
 app.Run();
