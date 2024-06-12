55406ad3793b44d8f818cbdb62ba304fb0c42c04
Ceydag <100134625+Ceydag@users.noreply.github.com>
2024-06-12 16:02:02 +0200

    api endpoint added

--- Album.Api/Services/AlbumService.cs
+++ Album.Api/Services/AlbumService.cs
@@ -23,13 +23,13 @@ public class AlbumService : IAlbumService
 	}
 	
 	public async Task<List<Album.Api.Models.Album>> GetAllAlbums()
-    {
-        if (_context.Albums != null)
-        {
-            return await _context.Albums.ToListAsync();
-        }
-        return new List<Album.Api.Models.Album>();
-    }
+	{
+		if (_context.Albums != null)
+		{
+			return await _context.Albums.ToListAsync();
+		}
+		return new List<Album.Api.Models.Album>();
+	}
 	
 	public async Task<Album.Api.Models.Album> GetAlbumById(int id)
 	{
@@ -67,154 +67,156 @@ public class AlbumService : IAlbumService
 
 namespace Album.Api.Controllers
 {
-    public class AlbumController : Controller
-    {
-        private readonly AlbumContext _context;
+	[ApiController]
+    [Route("api/album")]
+	public class AlbumController : Controller
+	{
+		private readonly AlbumContext _context;
 
-        public AlbumController(AlbumContext context)
-        {
-            _context = context;
-        }
+		public AlbumController(AlbumContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Album
-        public async Task<IActionResult> Index()
-        {
-              return _context.Albums != null ? 
-                          View(await _context.Albums.ToListAsync()) :
-                          Problem("Entity set 'AlbumContext.Albums'  is null.");
-        }
+		// GET: Album
+		public async Task<IActionResult> Index()
+		{
+			  return _context.Albums != null ? 
+						  View(await _context.Albums.ToListAsync()) :
+						  Problem("Entity set 'AlbumContext.Albums'  is null.");
+		}
 
-        // GET: Album/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Albums == null)
-            {
-                return NotFound();
-            }
+		// GET: Album/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Albums == null)
+			{
+				return NotFound();
+			}
 
-            var album = await _context.Albums
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (album == null)
-            {
-                return NotFound();
-            }
+			var album = await _context.Albums
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (album == null)
+			{
+				return NotFound();
+			}
 
-            return View(album);
-        }
+			return View(album);
+		}
 
-        // GET: Album/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Album/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Album/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Artist,ImageUrl")] Models.Album album)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(album);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(album);
-        }
+		// POST: Album/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Artist,ImageUrl")] Models.Album album)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(album);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(album);
+		}
 
-        // GET: Album/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Albums == null)
-            {
-                return NotFound();
-            }
+		// GET: Album/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Albums == null)
+			{
+				return NotFound();
+			}
 
-            var album = await _context.Albums.FindAsync(id);
-            if (album == null)
-            {
-                return NotFound();
-            }
-            return View(album);
-        }
+			var album = await _context.Albums.FindAsync(id);
+			if (album == null)
+			{
+				return NotFound();
+			}
+			return View(album);
+		}
 
-        // POST: Album/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Artist,ImageUrl")] Models.Album album)
-        {
-            if (id != album.Id)
-            {
-                return NotFound();
-            }
+		// POST: Album/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Artist,ImageUrl")] Models.Album album)
+		{
+			if (id != album.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(album);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AlbumExists(album.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(album);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(album);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!AlbumExists(album.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(album);
+		}
 
-        // GET: Album/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Albums == null)
-            {
-                return NotFound();
-            }
+		// GET: Album/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Albums == null)
+			{
+				return NotFound();
+			}
 
-            var album = await _context.Albums
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (album == null)
-            {
-                return NotFound();
-            }
+			var album = await _context.Albums
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (album == null)
+			{
+				return NotFound();
+			}
 
-            return View(album);
-        }
+			return View(album);
+		}
 
-        // POST: Album/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Albums == null)
-            {
-                return Problem("Entity set 'AlbumContext.Albums'  is null.");
-            }
-            var album = await _context.Albums.FindAsync(id);
-            if (album != null)
-            {
-                _context.Albums.Remove(album);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Album/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Albums == null)
+			{
+				return Problem("Entity set 'AlbumContext.Albums'  is null.");
+			}
+			var album = await _context.Albums.FindAsync(id);
+			if (album != null)
+			{
+				_context.Albums.Remove(album);
+			}
+			
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool AlbumExists(int id)
-        {
-          return (_context.Albums?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+		private bool AlbumExists(int id)
+		{
+		  return (_context.Albums?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
